fcdf7c156b8945a19afd12a0c62646018103cd0e
Grokes <default1one@gmail.com>
2023-10-08 12:00:54 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmHomework
+{
+    internal interface IVehicle
+    {
+        public void Move();
+        public void Refill(int oilCount);
+    }
+    internal class Car : IVehicle
+    {
+        public int OilCount { get; private set; }
+
+        public Car(int oilCount = 1000)
+        {
+            OilCount = oilCount;
+        }
+        public void Move()
+        {
+            if (OilCount > 100)
+            {
+                Console.WriteLine("Машина едет");
+                OilCount -= 100;
+                return;
+            }
+            Console.WriteLine("Недостаточно топлива");
+        }
+
+        public void Refill(int oilCount)
+        {
+            OilCount += oilCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using AlgorithmHomework;
+
+IVehicle test = new Car(500);
+
+test.Move();
+test.Move();
+test.Move();
+test.Move();
+test.Move();
+test.Refill(200);
+test.Move();
+test.Move();
